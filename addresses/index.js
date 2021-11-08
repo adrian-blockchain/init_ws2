5938b8b735db654b76431fc62669a782878da48d
Adrian KH <adrian.kinseyhillou@garageisep.com>
2021-11-08 18:09:09 +0100

    first commit

---
+++ addresses/index.js
@@ -0,0 +1,12 @@
+const pancakeSwapMainnet = require('./pancakeSwap-mainnet.json');
+const bakerySwapMainnet = require('./bakerySwap-mainnet.json');
+const tokensMainnet = require('./tokens-mainnet.json')
+
+
+module.exports = {
+    mainnet: {
+        pancakeSwap: pancakeSwapMainnet,
+        bakerySwap: bakerySwapMainnet,
+        tokens: tokensMainnet
+    }
+};
---
+++ contracts/Flashloan.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.8.6;
+
+/*
+    Vous viendrez coder ici votre premier smart contract
+*/
